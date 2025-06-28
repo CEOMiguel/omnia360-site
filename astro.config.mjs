2649bd0a6db66eeb941de7f99b1abe1b6ae7ddfa
Miguel <Mreyesc@omnia360.cl>
2025-06-27 20:52:58 -0400

    🔧 Elimina integración inválida astro-cname y mantiene configuración para dominio personalizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import cname from 'astro-cname';
 
 export default defineConfig({
   site: 'https://www.omnia360.cl',
   base: '/',
-  integrations: [cname()],
+  
 });
