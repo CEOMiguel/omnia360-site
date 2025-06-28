775191fa341b3a0e0a91cddf5b4a1abe0a0afe81
Miguel <Mreyesc@omnia360.cl>
2025-06-27 20:48:37 -0400

    🚀 Forzando redeploy con dominio personalizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import cname from 'astro-cname';
 
 export default defineConfig({
+  site: 'https://www.omnia360.cl',
   base: '/',
+  integrations: [cname()],
 });
